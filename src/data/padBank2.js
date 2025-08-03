a8d92ff200d2685cb8f93171514dc99610bf2ff0
Bailey J. Mora <158508797+BJ-Mora@users.noreply.github.com>
2025-08-02 17:06:21 -0700

    made changes to fix path issue with sounds stored in public folder

--- src/data/padBank2.js
+++ src/data/padBank2.js
@@ -1,11 +1,13 @@
+const BASE_URL = import.meta.env.BASE_URL || '/';
+
 export const padBank2 = [
-    { key: 'Q', sound: 'kick-1', url: '/sounds/bank2Sounds/kick-1.wav' },
-    { key: 'W', sound: 'kick-2', url: '/sounds/bank2Sounds/kick-2.wav' },
-    { key: 'E', sound: 'perc-1', url: '/sounds/bank2Sounds/perc-1.wav' },
-    { key: 'A', sound: 'hihat-1', url: '/sounds/bank2Sounds/hihat-1.wav' },
-    { key: 'S', sound: 'hihat-2', url: '/sounds/bank2Sounds/hihat-2.wav' },
-    { key: 'D', sound: 'clap-1', url: '/sounds/bank2Sounds/clap-1.wav' },
-    { key: 'Z', sound: 'stab-1', url: '/sounds/bank2Sounds/stab-1.wav' },
-    { key: 'X', sound: 'wub-2', url: '/sounds/bank2Sounds/wub-1.wav' },
-    { key: 'C', sound: 'phrase-1', url: '/sounds/bank2Sounds/phrase-1.wav' }
+  { key: 'Q', sound: 'kick-1', url: `${BASE_URL}sounds/bank2Sounds/kick-1.wav` },
+  { key: 'W', sound: 'kick-2', url: `${BASE_URL}sounds/bank2Sounds/kick-2.wav` },
+  { key: 'E', sound: 'perc-1', url: `${BASE_URL}sounds/bank2Sounds/perc-1.wav` },
+  { key: 'A', sound: 'hihat-1', url: `${BASE_URL}sounds/bank2Sounds/hihat-1.wav` },
+  { key: 'S', sound: 'hihat-2', url: `${BASE_URL}sounds/bank2Sounds/hihat-2.wav` },
+  { key: 'D', sound: 'clap-1', url: `${BASE_URL}sounds/bank2Sounds/clap-1.wav` },
+  { key: 'Z', sound: 'stab-1', url: `${BASE_URL}sounds/bank2Sounds/stab-1.wav` },
+  { key: 'X', sound: 'wub-2',  url: `${BASE_URL}sounds/bank2Sounds/wub-1.wav` },
+  { key: 'C', sound: 'phrase-1', url: `${BASE_URL}sounds/bank2Sounds/phrase-1.wav` }
 ];
